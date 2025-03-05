95beb901c64dc305b523c2dee9874c442d9580e2
Daniel <danielalves672@gmail.com>
2025-03-05 19:04:28 -0300

    projetos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btns = document.querySelectorAll("a");
+
+function prevent(evento) {
+  evento.preventDefault(); // Prevents default button behavior
+  window.open(this.href, "_blank");
+}
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", prevent);
+});
